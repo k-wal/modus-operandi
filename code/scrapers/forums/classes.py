9de3e492b5e423a46695f66c1d58887202c2f4ec
anshita <stellaolive157@gmail.com>
2022-12-02 19:04:31 +0100

    adding scraper for comments

--- code/scrapers/forums/classes.py
+++ code/scrapers/forums/classes.py
@@ -1,21 +1,56 @@
 # generic class for thread for all forums
 class Thread():
-	def __init__(self, url, title='sample title', content='', views=0, comments=0, created_by='sample user', date='00-00-00'):
+	def __init__(self, url, title='sample title', content='', views=0, comments=0, username='sample user', user_id = '000',
+		user_url = '', date='00-00-00',forum='', thread_id=000):
 		self.title = title
 		self.content = content
 		self.views = views
 		self.date = date
 		self.url = url
-		self.created_by = created_by
+		self.username = username
+		self.user_id = user_id
+		self.user_url = user_url
 		self.comments = comments
+		self.thread_id = thread_id
+		self.forum = forum
 
 	def to_dict(self):
 		return {
-		'url' : self.url,
+		'thread_id' : self.thread_id,
+		'forum' : self.forum,
 		'title' : self.title,
 		'content' : self.content,
 		'views' : self.views,
 		'comments' : self.comments,
-		'created_by' : self.created_by,
+		'url' : self.url,
+		'username' : self.username,
+		'user_id' : self.user_id,
+		'user_url' : self.user_url,
 		'date' : self.date
+		}
+
+class Comment():
+	def __init__(self, content='', comment_id=000, username='sample user', user_url='', user_id = '000', date='00-00-00', forum='',
+		thread_id=000, thread_url=''):
+		self.content = content
+		self.date = date
+		self.thread_url = thread_url
+		self.thread_id = thread_id
+		self.username = username
+		self.user_id = user_id
+		self.user_url = user_url
+		self.comment_id = comment_id
+		self.forum = forum
+
+	def to_dict(self):
+		return{
+		'date' : self.date,
+		'thread_url' : self.thread_url,
+		'thread_id' : self.thread_id,
+		'content' : self.content,
+		'username' : self.username,
+		'user_id' : self.user_id,
+		'user_url' : self.user_url,
+		'comment_id' : self.comment_id,
+		'forum' : self.forum
 		}
--- code/scrapers/forums/drugsforum.py
+++ code/scrapers/forums/drugsforum.py
@@ -20,17 +20,28 @@ class DrugsForumScraper():
 		self.dir_path = '../../../corpus/forums/drugsforum'
 
 	# write list of Thread objects in a csv file
-	def write_threads(self, threads):
+	def write_threads(self, threads, page):
 		dir_path = self.dir_path + '/threads'
 		if not os.path.isdir(dir_path):
 			os.makedirs(dir_path)
 		df = pd.DataFrame.from_records([t.to_dict() for t in threads])
-		df.to_csv(dir_path + '/all.csv', sep=',')
+		
+		if page == 1:
+			df.to_csv(dir_path + '/all.csv', index=False, sep=',')
+		else:
+			df.to_csv(dir_path + '/all.csv', mode='a', index=False, header=False, sep=',')
 
+	# write list of Comment objects in a csv file
+	def write_comments(self, comments):
+		dir_path = self.dir_path + '/comments'
+		if not os.path.isdir(dir_path):
+			os.makedirs(dir_path)
+		df = pd.DataFrame.from_records([c.to_dict() for c in comments])
+		df.to_csv(dir_path + '/all.csv', index=False, sep=',')
 
 	# get threads and their details and call write function
-	def get_threads(self):
-		page = 1
+	def get_threads(self, page=1):
+		print("page: ", page)
 		url = 'https://drugsforum.nl/forums/research-chemicals.37/page-'+str(page)+'?order=post_date&direction=desc'
 		threads = []
 		r = requests.get(url)
@@ -44,9 +55,23 @@ class DrugsForumScraper():
 			
 			title_div = div.find('div', class_='structItem-title')
 			thread_url = self.url + title_div.find('a')['href']
+			try:
+				thread_id = int(thread_url.split('.')[-1].replace('/',''))
+			except:
+				print("ID ERROR")
+				continue
+
 			thread_title = title_div.text.strip()
 
-			thread_created_by = div.find('a', class_='username').text.strip()
+			try:
+				thread_username = div.find('a', class_='username').text.strip()
+				thread_user_url = self.url + div.find('a', class_='username')['href']
+				thread_user_id = int(thread_user_url.split('/')[-2].split('.')[1])
+				#print(type(thread_user_id))
+			except:
+				print("CREATED BY ERROR")
+				continue
+
 			thread_date = div.find('li', class_='structItem-startDate').find('time')['datetime'][0:10]
 
 			thread_comments = int(detail_div.findAll('dd')[0].text)
@@ -58,16 +83,68 @@ class DrugsForumScraper():
 			threads.append(DrugsForumThread(
 				url = thread_url,
 				title = thread_title,
-				created_by = thread_created_by,
+				username = thread_username,
+				user_id = thread_user_id,
+				user_url = thread_user_url,
 				date = thread_date,
 				views = thread_views,
-				comments = thread_comments
+				comments = thread_comments,
+				thread_id = thread_id,
+				forum = 'drugsforum'
 			))
 
 		for i,thread in enumerate(threads):
 			thread.get_thread_content()
-			print(i,thread.title,thread.date)
-		self.write_threads(threads)
+			print(i,thread.thread_id, thread.title)
+		self.write_threads(threads, page)
+
+	def get_comments_of_thread(self, thread_url):
+		comments = []
+		r = requests.get(thread_url)
+		soup = BeautifulSoup(r.content, 'html.parser')
+		articles = soup.findAll('article', class_='message--post')
+
+		for article in articles:
+			#comment_username = article['data-author']
+			h4 = article.find('h4')
+			comment_user_url = self.url + h4.find('a')['href']
+			comment_username = h4.text
+			comment_user_id = h4.find('a')['data-user-id']
+
+			comment_id = int(article['data-content'].split('-')[-1])
+			comment_date = article.find('time')['datetime'][0:10]
+
+			thread_id = int(thread_url.split('.')[-1].replace('/',''))
+
+			# getting content of comment and removing quoted parts from it
+			message_body = article.find('article', class_="message-body")
+			quoted = []
+			for t,c in zip(message_body.findAll('div', class_='bbCodeBlock-title'), message_body.findAll('div', class_='bbCodeBlock-content')):
+				quoted.append(t.text)
+				quoted.append(c.text)
+
+			content = message_body.text
+			for q in quoted:
+				content = content.replace(q, '')
+			content = content.replace('\n', ' ')
+
+			comments.append(Comment(
+				user_url = comment_user_url,
+				user_id = comment_user_id,
+				username = comment_username,
+				comment_id = comment_id,
+				thread_id = thread_id,
+				thread_url = thread_url, 
+				content = content, 
+				date = comment_date,
+				forum = 'drugsforum'))
+			print(comment_username)
+		self.write_comments(comments)	
+
 
 scraper = DrugsForumScraper()
-scraper.get_threads()
+# for i in range(1,5):
+# 	scraper.get_threads(i)
+
+url = 'https://drugsforum.nl/threads/4f-mph-of-3-cmc.73388/'
+scraper.get_comments_of_thread(url)
